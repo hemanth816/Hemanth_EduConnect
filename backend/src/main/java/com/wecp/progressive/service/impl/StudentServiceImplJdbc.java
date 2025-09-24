b63b92e85c9965d956eaa9b7b48c80724b0b915b
hemanth816 <maligahemanthkumar@gmail.com>
2025-09-25 00:35:06 +0530

    Update StudentServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/StudentServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/StudentServiceImplJdbc.java
@@ -1,5 +1,36 @@
 package com.wecp.progressive.service.impl;
+import java.util.*;
 
-public class StudentServiceImplJdbc  {
+import com.wecp.progressive.dao.StudentDAO;
+import com.wecp.progressive.entity.Student;
+import com.wecp.progressive.service.StudentService;
+public class StudentServiceImplJdbc  implements StudentService{
+    private StudentDAO studentDAO;
+    
+    public StudentServiceImplJdbc(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
+    }
+    public List<Student> getAllStudents()
+    {
+        List<Student> list=new ArrayList<>();
+        return list;
+    }
+    public Integer addStudent(Student student)
+    {
+        return -1;
+    }
+    public List<Student> getAllStudentSortedByName()
+    {
+        List<Student> list=new ArrayList<>();
+        return list;
+    }
+   public void deleteStudent(int studentId)
+   {
+    
+   }
 
+   public Student getStudentById(int studentId)
+   {
+    return null;
+   }
 }
