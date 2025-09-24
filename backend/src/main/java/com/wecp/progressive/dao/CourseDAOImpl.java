83d703c530b610dc58e2e7608714686e1004f12b
hemanth816 <maligahemanthkumar@gmail.com>
2025-09-25 00:30:13 +0530

    Update CourseDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/CourseDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/CourseDAOImpl.java
@@ -1,5 +1,35 @@
 package com.wecp.progressive.dao;
 
-public class CourseDAOImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wecp.progressive.entity.Course;
+
+public class CourseDAOImpl implements CourseDAO {
+
+public int addCourse(Course course)
+{
+    return -1;
+
+}
+   public  Course getCourseById(int courseId)
+    {
+        return null;
+
+
+    }
+    public void updateCourse(Course course)
+    {
+        
+    }
+    public void deleteCourse(int courseId)
+    {
+
+    }
+   public  List<Course> getAllCourses()
+    {
+      //  List<Course> list=new ArrayList<Course>();
+        return List.of();
+    }
 
 }
