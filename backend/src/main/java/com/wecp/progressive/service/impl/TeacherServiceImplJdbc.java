ad209211ab188ef13318b75ad2970d697b024a20
hemanth816 <maligahemanthkumar@gmail.com>
2025-09-25 00:36:20 +0530

    Update TeacherServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/TeacherServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeacherServiceImplJdbc.java
@@ -1,5 +1,45 @@
 package com.wecp.progressive.service.impl;
 
-public class TeacherServiceImplJdbc  {
+import java.util.ArrayList;
+import java.util.*;
+
+import com.wecp.progressive.dao.TeacherDAO;
+import com.wecp.progressive.entity.Teacher;
+import com.wecp.progressive.service.TeacherService;
+
+public class TeacherServiceImplJdbc implements TeacherService  {
+    private TeacherDAO teacheDAO;
+          
+        public TeacherServiceImplJdbc(TeacherDAO teacheDAO) {
+        this.teacheDAO = teacheDAO;
+    }
+        public List<Teacher> getAllTeachers()
+    {
+   List<Teacher>list=new ArrayList<>();
+return list;
+    }
+    public Integer addTeacher(Teacher teacher)
+    {
+        return-1;
+    }
+public List<Teacher> getTeacherSortedByExperience()
+{
+    List<Teacher>list=new ArrayList<>();
+return list;
+}
+public void updateTeacher(Teacher teacher)
+{
+
+}
+
+public void deleteTeacher(Teacher teacher)
+{
+    
+}
+public Teacher getTeacherById(int teacherId)
+{
+    return null;
+}
+
 
 }
